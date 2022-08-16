2e8449b6da5b15330765aedb417a4206f6262731
codessi <johansuh@gmail.com>
2022-08-16 15:24:38 -0700

    125 done

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,9 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { auth } from "../config";
 import { AuthContext } from "../contexts/AuthContext";
 
 export const useLogout = () => {
+  const [isCanceled, setIsCanceled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useContext(AuthContext);
@@ -14,15 +15,25 @@ export const useLogout = () => {
     try {
       await auth.signOut();
       dispatch({ type: "LOGOUT" });
-      setError(null)
-      setIsPending(false)
 
+      if (!isCanceled) {
+        setError(null);
+        setIsPending(false);
+      }
     } catch (err) {
-      console.log(err.message)
-      setError(err.message);
-      setIsPending(false);
+      if (!isCanceled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    return () => {
+      setIsCanceled(true);
+    };
+  }, []);
 
   return { error, isPending, logout };
 };
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,43 +1,48 @@
-import { useContext, useState } from "react"
-import { auth } from './../config'
-import { AuthContext } from "../contexts/AuthContext"
-
+import { useContext, useEffect, useState } from "react";
+import { auth } from "./../config";
+import { AuthContext } from "../contexts/AuthContext";
 
 export function useSignup() {
-
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useContext(AuthContext)
+  const [isCanceled, setIsCanceled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useContext(AuthContext);
   // error, isPending, signup??
-  const signup = async(email, password, displayName) => {
-    setError(null)
-    setIsPending(true)
-    
+  const signup = async (email, password, displayName) => {
+    setError(null);
+    setIsPending(true);
 
-    try { 
-      const res = await auth.createUserWithEmailAndPassword(email, password)
-      
-      console.log(res.user)
+    try {
+      const res = await auth.createUserWithEmailAndPassword(email, password);
+
+      console.log(res.user);
 
       if (!res) {
-        throw new Error("Could not complete signup")
+        throw new Error("Could not complete signup");
       }
 
-      await res.user?.updateProfile({ displayName })
+      await res.user?.updateProfile({ displayName });
 
       // dispatch and update the state
-      dispatch({type: 'LOGIN', payload: res.user})
+      dispatch({ type: "LOGIN", payload: res.user });
 
-      setIsPending(false)
-      setError(null)
+      if (!isCanceled) {
+        setError(null);
+        setIsPending(false);
+      }
+    } catch (err) {
+      if (!isCanceled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
     }
-    catch (error) {
-      console.log(error.message)
-      setError(error.message)
-      setIsPending(false)
-    }
-  }
+  };
+  useEffect(() => {
+    return () => {
+      setIsCanceled(true);
+    };
+  }, []);
 
-  return {error, isPending, signup}
-
+  return { error, isPending, signup };
 }
