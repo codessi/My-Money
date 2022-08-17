f12a0a18cde4cd21680797e6f18fce6822a0b7ad
codessi <johansuh@gmail.com>
2022-08-17 14:53:16 -0700

    131 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import './App.css'
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import React from "react";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
@@ -9,7 +9,8 @@ import Navbar from "./components/Navbar";
 import useAuthContext from "./hooks/useAuthContext";
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
+ 
 
   return (
     <div className="app">
@@ -18,15 +19,18 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              <Home />
+              {!user && <Redirect to='/login'/>}
+              {user && <Home />}
             </Route>
 
             <Route path="/login">
-              <Login />
+              {user && <Redirect to='/'/>}
+              {!user && <Login />}
             </Route>
 
             <Route path="/signup">
-              <Signup />
+            {user && <Redirect to='/'/>}
+              {!user && <Signup />}
             </Route>
           </Switch>
         </Router>
