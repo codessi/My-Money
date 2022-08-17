ffd98047cd5201cd40d10373502460807e75aa52
codessi <johansuh@gmail.com>
2022-08-17 15:16:42 -0700

    132 done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
-import style from "./Home.module.css";
+import styles from "./Home.module.css";
 import React from "react";
 
 
 const Home = () => {
   return (
-    <div className="app">
-      home
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction List</div>
+      <div className={styles.sidebar}></div>
     </div>
   );
 };
